6a972bca05fc50b29a0f57041aa5aa45c3eedc59
Pavel Mezencev <mezencev24@mail.ru>
2022-03-26 10:58:16 +0300

    add data export

--- src/components/context/FeedbackProvider.jsx
+++ src/components/context/FeedbackProvider.jsx
@@ -1,12 +1,11 @@
 import { createContext, useState } from 'react'
 import nanoId from 'nano-id'
+import data from '../../data/data'
 
 const FeedbackContext = createContext()
 
 export const FeebdackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: '1', text: 'This is the text', rating: 5 },
-  ])
+  const [feedback, setFeedback] = useState(data)
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure want to delete?')) {
